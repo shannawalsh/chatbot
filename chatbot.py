027d794943d397d37c6659c59b2ef36cb64646f9
Shanna <shanna.r.walsh@gmail.com>
2024-06-11 16:56:39 -0500

    refactor by adding a function

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,19 @@ from openai import OpenAI
 
 client = OpenAI()
 
+def get_response_from_openai_chatbot(model, messages):
+    api_response = client.chat.completions.create(
+            model = model,
+            messages = messages
+        )
+
+    #extract the message content 
+    response_content = api_response.choices[0].message.content
+
+    #return the response text 
+    return response_content
+    
+# ask the user for input
 user_input = input("\nAsk something...\n\n")
 
 model = "gpt-3.5-turbo"
@@ -9,14 +22,8 @@ model = "gpt-3.5-turbo"
 messages = [
     {"role": "system", "content": "You are an assistant that always answers in the form of a poem."},
     {"role": "user", "content": user_input}
-]
+    ]
 
-response = client.chat.completions.create(
-    model = model,
-    messages = messages
-)
+response_for_user = get_response_from_openai_chatbot(model, messages)
 
-response_for_user = response.choices[0].message.content
 print("\n" + response_for_user + "\n")
-
-
